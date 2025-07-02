9c266f1a130aa7e370a554e7620f6c8a5122d62f
bragi <spontaneousmanji@gmail.com>
2025-07-02 07:57:12 +0100

    chore: update .gitIgnore to include .env file and add TODO comment for server port configuration

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import http from "http";
 import app from "./app.js";
 
-const port = process.env.PORT || "3000";
+const port = process.env.PORT || "3000"; //TODO : RUN SERVER ON PORT 8000
 const server = http.createServer(app);
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
